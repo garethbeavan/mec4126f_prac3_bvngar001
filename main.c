42868ee4e8bd3da271ff0426d7ea4c54f176feb5
garethbeavan <111308078+garethbeavan@users.noreply.github.com>
2023-03-06 09:14:39 +0200

    Q3.2 - Display age to LCD

--- main.c
+++ main.c
@@ -22,15 +22,30 @@ struct age_data my_age;
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
 void main(void);                                                   //COMPULSORY
+void init_LCD(void);
+void delay();
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 void main(void)
 {
+	my_age.day = 21;
+	my_age.month = 11;
+	my_age.year = 2001;
+	my_age.age = 21;
+
+	init_LCD();
+	char string[3];
 
 	while(1)
 	{
-
+		for(int i=0; i<= my_age.age; i++)
+		{
+			lcd_command(CLEAR);
+			sprintf(string, "%d", i);
+			lcd_putstring(string);
+			delay(120000);
+		}
 	}
 }
 
