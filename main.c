ad7fee98c79b38b747b00420ca1d28e6e635e7c3
garethbeavan <111308078+garethbeavan@users.noreply.github.com>
2023-03-06 09:07:28 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+// Description----------------------------------------------------------------|
+/*
+ *
+ */
+// DEFINES AND INCLUDES-------------------------------------------------------|
+
+#define STM32F051												   //COMPULSORY
+#include "stm32f0xx.h"											   //COMPULSORY
+#include "lcd_stm32f0.h"
+#include <stdio.h>
+// GLOBAL VARIABLES ----------------------------------------------------------|
+
+
+
+// FUNCTION DECLARATIONS -----------------------------------------------------|
+
+void main(void);                                                   //COMPULSORY
+
+// MAIN FUNCTION -------------------------------------------------------------|
+
+void main(void)
+{
+
+	while(1)
+	{
+
+	}
+}
+
+// OTHER FUNCTIONS -----------------------------------------------------------|
+
+
+
